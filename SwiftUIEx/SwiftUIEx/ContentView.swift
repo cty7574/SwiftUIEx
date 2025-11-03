630440389cbffc21b6c724f008cc26642ea74498
cty7574 <vicpool@naver.com>
2025-11-03 14:55:52 +0900

    파일 정리

--- SwiftUIEx/SwiftUIEx/ContentView.swift
+++ SwiftUIEx/SwiftUIEx/ContentView.swift
@@ -10,24 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            // Liquid Glass Button
-            Button("Tap Me") {
-                
-            }
-            .buttonStyle(.glass)
-            
-            // 커스텀 뷰에 liquid 스타일 적용
-            Image(systemName: "suit.heart.fill")
-                .font(.largeTitle)
-                .foregroundStyle(.red.gradient)
-                .frame(width: 50, height: 50)
-                // 상호작용 가능한 glass effect
-                //.glassEffect(.regular.interactive(), in: .circle)
-                .glassEffect(.regular.tint(.red.opacity(0.35)).interactive(), in: .circle) // 배경색 적용
-            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.gray)
+        .padding()
     }
 }
 
