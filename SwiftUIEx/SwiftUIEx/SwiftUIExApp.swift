1e32f8877d16368573de2e1796d2b920381c9ec5
cty7574 <vicpool@naver.com>
2025-11-03 14:48:18 +0900

    Liquid Glass View

---
+++ SwiftUIEx/SwiftUIEx/SwiftUIExApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExApp.swift
+//  SwiftUIEx
+//
+//  Created by 멘태 on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
