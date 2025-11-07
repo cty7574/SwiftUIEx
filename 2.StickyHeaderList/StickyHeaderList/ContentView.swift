42bba2f5daa58b59a757e1fd2f732675b588c938
cty7574 <vicpool@naver.com>
2025-11-07 17:23:59 +0900

    sticky header 프로젝트 생성

---
+++ 2.StickyHeaderList/StickyHeaderList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StickyHeaderList
+//
+//  Created by 멘태 on 11/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2.StickyHeaderList/StickyHeaderList/StickyHeaderListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderListApp.swift
+//  StickyHeaderList
+//
+//  Created by 멘태 on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
