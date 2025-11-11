494dc43226a8b2e5aaf80027d80fd791c7c6ec0a
cty7574 <vicpool@naver.com>
2025-11-11 20:10:20 +0900

    모델 정의

---
+++ 3.OnBoarding/OnBoarding/Models/OnBoardingCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingCoordinator.swift
+//  OnBoarding
+//
+//  Created by 멘태 on 11/11/25.
+//
+
+import SwiftUI
+
+@Observable
+final class OnBoardingCoordinator {
+    var items: [OnBoardingItem] = []
+    var overlayWindow: UIWindow?
+    
+    var orderedItem: [OnBoardingItem] {
+        return items.sorted { $0.id < $1.id }
+    }
+}
---
+++ 3.OnBoarding/OnBoarding/Models/OnBoardingItem.swift
@@ -0,0 +1,14 @@
+//
+//  OnBoardingItem.swift
+//  OnBoarding
+//
+//  Created by 멘태 on 11/11/25.
+//
+
+import SwiftUI
+
+struct OnBoardingItem: Identifiable {
+    var id: Int
+    var view: AnyView
+    var maskLocation: CGRect
+}
