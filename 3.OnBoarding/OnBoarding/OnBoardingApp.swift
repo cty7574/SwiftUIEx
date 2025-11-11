3a6eaed39d12e6a983075840e32bd7d2a876a961
cty7574 <vicpool@naver.com>
2025-11-11 20:05:53 +0900

    프로젝트 생성

---
+++ 3.OnBoarding/OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by 멘태 on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
