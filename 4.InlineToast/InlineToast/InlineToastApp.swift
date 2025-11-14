3a0a8131f577cc4b784696f27dbf29ccfb159bc7
cty7574 <vicpool@naver.com>
2025-11-14 17:01:43 +0900

    프로젝트 생성

---
+++ 4.InlineToast/InlineToast/InlineToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  InlineToastApp.swift
+//  InlineToast
+//
+//  Created by 멘태 on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct InlineToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
