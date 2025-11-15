b3b8c7cbcd0bed989a52fd1ede26b425d64814c9
cty7574 <vicpool@naver.com>
2025-11-15 18:30:26 +0900

    프로젝트 생성

---
+++ 5.CustomKeyboard/CustomKeyboard/CustomKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomKeyboardApp.swift
+//  CustomKeyboard
+//
+//  Created by 멘태 on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
