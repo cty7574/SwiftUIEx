17464bb2643401d4b58eccb702a098dc8e7da7a6
cty7574 <vicpool@naver.com>
2025-11-24 22:14:26 +0900

    프로젝트 생성: 6.일정관리

---
+++ 6.TaskManagementApp/TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by 멘태 on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
