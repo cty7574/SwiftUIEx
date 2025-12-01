b1afc59f178cc4f71fce5c0689ce423ce9ffad9e
cty7574 <vicpool@naver.com>
2025-12-01 21:51:42 +0900

    프로젝트 생성 7.Dynamic Chips UI

---
+++ 7.DynamicChipsUI/DynamicChipsUI/DynamicChipsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicChipsUIApp.swift
+//  DynamicChipsUI
+//
+//  Created by 멘태 on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicChipsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
