37054aa7a9ce6bd4066a2946d41c9fa6fd4137dc
cty7574 <vicpool@naver.com>
2025-12-03 18:44:11 +0900

    ChipsView가 필요한 만큼만 높이를 차지하도록 수정

--- 8.ChipsSelection/ChipsSelection/Views/ChipsView.swift
+++ 8.ChipsSelection/ChipsSelection/Views/ChipsView.swift
@@ -27,7 +27,9 @@ fileprivate struct CustomChipLayout: Layout {
     var spacing: CGFloat
     
     func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        return proposal.replacingUnspecifiedDimensions()
+        let width = proposal.width ?? 0
+        
+        return CGSize(width: width, height: maxHeight(proposal: proposal, subviews: subviews))
     }
     
     func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
@@ -47,6 +49,29 @@ fileprivate struct CustomChipLayout: Layout {
                 origin.x += fitSize.width + spacing
             }
         }
+    }
+    
+    private func maxHeight(proposal: ProposedViewSize, subviews: Subviews) -> CGFloat {
+        var origin: CGPoint = .zero
+        
+        for subview in subviews {
+            let fitSize = subview.sizeThatFits(proposal)
+            
+            if (origin.x + fitSize.width) > (proposal.width ?? 0) {
+                origin.x = 0
+                origin.y += fitSize.height + spacing
+                
+                origin.x += fitSize.width + spacing
+            } else {
+                origin.x += fitSize.width + spacing
+            }
+            
+            if subview == subviews.last {
+                origin.y += fitSize.height
+            }
+        }
+        
+        return origin.y
     }
 }
 
