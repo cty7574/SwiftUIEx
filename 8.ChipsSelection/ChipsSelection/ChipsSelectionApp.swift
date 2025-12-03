ecbf16e98685b142861f770effa7b86bbe921a13
cty7574 <vicpool@naver.com>
2025-12-03 18:25:23 +0900

    프로젝트 생성: 8.Chips Selection

---
+++ 8.ChipsSelection/ChipsSelection/ChipsSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipsSelectionApp.swift
+//  ChipsSelection
+//
+//  Created by 멘태 on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ChipsSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
