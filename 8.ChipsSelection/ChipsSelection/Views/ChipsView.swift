587b6a5d592a87e1b7caf581cb1e05bd8fa3408d
cty7574 <vicpool@naver.com>
2025-12-03 18:39:51 +0900

    ChipsView에 커스텀 레이아웃 적용

--- 8.ChipsSelection/ChipsSelection/Views/ChipsView.swift
+++ 8.ChipsSelection/ChipsSelection/Views/ChipsView.swift
@@ -15,7 +15,38 @@ struct ChipsView<Content: View, Tag: Equatable>: View where Tag: Hashable {
     @State private var selectedTags: [Tag] = []
     
     var body: some View {
-        Text("Hello, world!")
+        CustomChipLayout(spacing: spacing) {
+            ForEach(tags, id: \.self) { tag in
+                content(tag, selectedTags.contains(tag))
+            }
+        }
+    }
+}
+
+fileprivate struct CustomChipLayout: Layout {
+    var spacing: CGFloat
+    
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        return proposal.replacingUnspecifiedDimensions()
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        var origin = bounds.origin
+        
+        for subview in subviews {
+            let fitSize = subview.sizeThatFits(proposal)
+            
+            if origin.x + fitSize.width > bounds.maxX {
+                origin.x = bounds.minX
+                origin.y += fitSize.height + spacing
+                
+                subview.place(at: origin, proposal: proposal)
+                origin.x += fitSize.width + spacing
+            } else {
+                subview.place(at: origin, proposal: proposal)
+                origin.x += fitSize.width + spacing
+            }
+        }
     }
 }
 
