06beee2f1e0fb447fe6e854ad916a94b554814db
cty7574 <vicpool@naver.com>
2025-12-05 14:22:13 +0900

    프로젝트 생성: 9.Interactive Map Carousel

---
+++ 9.InteractiveMapCarousel/InteractiveMapCarousel/InteractiveMapCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveMapCarouselApp.swift
+//  InteractiveMapCarousel
+//
+//  Created by 멘태 on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveMapCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
