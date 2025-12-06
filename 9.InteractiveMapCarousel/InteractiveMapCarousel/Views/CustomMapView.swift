e128279991740a678cabc0e40be99d184123729d
cty7574 <vicpool@naver.com>
2025-12-06 22:06:12 +0900

    로딩뷰, bottom carousel 기본 UI 구성

--- 9.InteractiveMapCarousel/InteractiveMapCarousel/Views/CustomMapView.swift
+++ 9.InteractiveMapCarousel/InteractiveMapCarousel/Views/CustomMapView.swift
@@ -14,6 +14,8 @@ struct CustomMapView: View {
     var lookupText: String
     var limit: Int
     
+    var animation: Animation { .smooth(duration: 0.45, extraBounce: 0) }
+    
     @State private var cameraPosition: MapCameraPosition
     @State private var places: [Place] = []
     
@@ -31,6 +33,24 @@ struct CustomMapView: View {
     var body: some View {
         NavigationStack {
             Map(position: $cameraPosition)
+                .overlay {
+                    loadingOverlay()
+                }
+                .safeAreaInset(edge: .bottom, spacing: 0, content: {
+                    GeometryReader { proxy in
+                        let size: CGSize = proxy.size
+                        
+                        bottomCarousel(size.width)
+                        
+                        if places.isEmpty {
+                            RoundedRectangle(cornerRadius: 30)
+                                .foregroundStyle(.white)
+                                .padding(.horizontal)
+                                .frame(width: size.width, height: size.height)
+                        }
+                    }
+                    .frame(height: 200)
+                })
                 .navigationTitle("Nearby Places")
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar {
@@ -52,6 +72,64 @@ struct CustomMapView: View {
                     }
                 }
         }
+        .onAppear {
+            guard places.isEmpty else { return }
+            fetchPlaces()
+        }
+    }
+    
+    @ViewBuilder
+    private func loadingOverlay() -> some View {
+        Rectangle()
+            .fill(.black.opacity(places.isEmpty ? 0.35 : 0))
+            .ignoresSafeArea()
+    }
+    
+    @ViewBuilder
+    private func bottomCarousel(_ width: CGFloat) -> some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 0) {
+                ForEach(places) { place in
+                    RoundedRectangle(cornerRadius: 30)
+                        .fill(.white)
+                        .padding(.horizontal)
+                        .frame(width: width)
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollIndicators(.hidden)
+        .scrollClipDisabled()
+        .scrollTargetBehavior(.paging)
+    }
+    
+    private func fetchPlaces() {
+        Task {
+            let request = MKLocalSearch.Request()
+            request.region = userRegion
+            request.naturalLanguageQuery = lookupText
+            
+            let search = MKLocalSearch(request: request)
+            if let items = try? await search.start().mapItems {
+                let places: [Place] = items.compactMap { item in
+                    let name = item.name ?? "Unknown"
+                    var coordinates: CLLocationCoordinate2D
+                    
+                    if #available(iOS 26, *) {
+                        coordinates = item.location.coordinate
+                    } else {
+                        coordinates = item.placemark.coordinate
+                    }
+                    
+                    return Place(name: name, coordinates: coordinates, mapItem: item)
+                }
+                    .prefix(limit).compactMap { $0 }
+                
+                withAnimation(animation) {
+                    self.places = places
+                }
+            }
+        }
     }
 }
 
@@ -59,6 +137,7 @@ struct CustomMapView: View {
     CustomMapView(
         userRegion: .applePark,
         userCoordinates: MKCoordinateRegion.applePark.center,
-        lookupText: "Starbucks"
+        lookupText: "Starbucks",
+        limit: 3
     )
 }
