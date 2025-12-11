1b1ab1411c1ee6ae89aefb4c9ac1c85d1557f0c3
cty7574 <vicpool@naver.com>
2025-12-11 23:20:30 +0900

    프로젝트 생성 10.LoginUI

---
+++ 10.LoginUI/LoginUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginUI
+//
+//  Created by 멘태 on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 10.LoginUI/LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by 멘태 on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
