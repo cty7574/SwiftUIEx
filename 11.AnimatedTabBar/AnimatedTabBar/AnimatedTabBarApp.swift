350c595d6eba6bd3a612d88bc01c331c9afa4de0
cty7574 <vicpool@naver.com>
2025-12-17 23:04:47 +0900

    프로젝트 생성 11.Animated TabBar

---
+++ 11.AnimatedTabBar/AnimatedTabBar/AnimatedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTabBarApp.swift
+//  AnimatedTabBar
+//
+//  Created by 멘태 on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
