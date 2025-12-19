647f42fb1628837e68c0382391e13e751851bedf
cty7574 <vicpool@naver.com>
2025-12-19 19:08:08 +0900

    프로젝트 생성: 12.Custom Async Button

---
+++ 12.CustomAsyncButton/CustomAsyncButton/CustomAsyncButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAsyncButtonApp.swift
+//  CustomAsyncButton
+//
+//  Created by 멘태 on 12/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomAsyncButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
