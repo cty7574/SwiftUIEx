edc21e6e3767616a091b38f161b0719734ec1f72
cty7574 <vicpool@naver.com>
2025-12-20 22:01:37 +0900

    커스텀 async 버튼 구현

---
+++ 12.CustomAsyncButton/CustomAsyncButton/AnimatedButton.swift
@@ -0,0 +1,74 @@
+//
+//  AnimatedButton.swift
+//  CustomAsyncButton
+//
+//  Created by 멘태 on 12/20/25.
+//
+
+import SwiftUI
+
+struct AnimatedButton: View {
+    var config: Config
+    var shape: AnyShape = .init(.capsule)
+    var onTap: () async -> ()
+    
+    @State private var isLoading: Bool = false
+    
+    var body: some View {
+        Button {
+            Task {
+                isLoading = true
+                await onTap()
+                isLoading = false
+            }
+        } label: {
+            HStack(spacing: 10) {
+                if let symbolImage = config.symbolImage {
+                    Image(systemName: symbolImage)
+                        .font(.title3)
+                        .transition(.blurReplace)
+                }
+                
+                if isLoading {
+                    SpinnerView(tint: config.foregroundColor, lineWidth: 4)
+                        .frame(width: 20, height: 20)
+                        .transition(.blurReplace)
+                }
+                
+                Text(config.title)
+                    .contentTransition(.interpolate)
+                    .fontWeight(.semibold)
+            }
+            .padding(.horizontal, config.hPadding)
+            .padding(.vertical, config.vPadding)
+            .foregroundStyle(config.foregroundColor)
+            .background(config.background.gradient)
+            .clipShape(shape)
+            .contentShape(shape)
+        }
+        .disabled(isLoading)
+        .buttonStyle(ScaleButtonStyle())
+        .animation(config.animation, value: config)
+        .animation(config.animation, value: isLoading)
+    }
+    
+    struct Config: Equatable {
+        var title: String
+        var foregroundColor: Color
+        var background: Color
+        var symbolImage: String?
+        var hPadding: CGFloat = 16
+        var vPadding: CGFloat = 10
+        var animation: Animation = .easeInOut(duration: 0.25)
+    }
+}
+
+fileprivate struct ScaleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .animation(.linear(duration: 0.2)) {
+                $0
+                    .scaleEffect(configuration.isPressed ? 0.9 : 1)
+            }
+    }
+}
--- 12.CustomAsyncButton/CustomAsyncButton/ContentView.swift
+++ 12.CustomAsyncButton/CustomAsyncButton/ContentView.swift
@@ -7,15 +7,59 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum TransactionState: String {
+    case idle = "Click to pay"
+    case analyzing = "Analyzing Transaction"
+    case processing = "Processing Transaction"
+    case completed = "Transaction Completed"
+    case failed = "Transaction Failed"
+    
+    var color: Color {
+        switch self {
+        case .idle: return .black
+        case .analyzing: return .blue
+        case .processing: return .orange
+        case .completed: return .green
+        case .failed: return .red
         }
-        .padding()
+    }
+    
+    var image: String? {
+        switch self {
+        case .idle: return "apple.logo"
+        case .analyzing: return nil
+        case .processing: return nil
+        case .completed: return "checkmark.circle.fill"
+        case .failed: return "xmark.circle.fill"
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var transactionState: TransactionState = .idle
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                let config: AnimatedButton.Config = AnimatedButton.Config(
+                    title: transactionState.rawValue,
+                    foregroundColor: .white,
+                    background: transactionState.color,
+                    symbolImage: transactionState.image
+                )
+                
+                AnimatedButton(config: config) {
+                    transactionState = .analyzing
+                    try? await Task.sleep(for: .seconds(3))
+                    transactionState = .processing
+                    try? await Task.sleep(for: .seconds(3))
+                    transactionState = .failed
+                    try? await Task.sleep(for: .seconds(1))
+                    transactionState = .idle
+                }
+            }
+            .navigationTitle("Custom Button")
+        }
     }
 }
 
