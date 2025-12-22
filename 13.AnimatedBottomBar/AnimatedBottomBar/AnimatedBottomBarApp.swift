2e3ef368a0d38471837f9a58e9d9ff095c927699
cty7574 <vicpool@naver.com>
2025-12-22 23:07:50 +0900

    프로젝트 생성: 13.Animated Bottom Bar

---
+++ 13.AnimatedBottomBar/AnimatedBottomBar/AnimatedBottomBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedBottomBarApp.swift
+//  AnimatedBottomBar
+//
+//  Created by 멘태 on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedBottomBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 13.AnimatedBottomBar/AnimatedBottomBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimatedBottomBar
+//
+//  Created by 멘태 on 12/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
