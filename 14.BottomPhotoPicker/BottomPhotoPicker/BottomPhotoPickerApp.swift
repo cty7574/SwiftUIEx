4ebd8d8da004b4543515a59d3628f450263ffe42
cty7574 <vicpool@naver.com>
2025-12-26 16:56:27 +0900

    프로젝트 생성: 14.Bottom Photo Picker

---
+++ 14.BottomPhotoPicker/BottomPhotoPicker/BottomPhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomPhotoPickerApp.swift
+//  BottomPhotoPicker
+//
+//  Created by 멘태 on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BottomPhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
