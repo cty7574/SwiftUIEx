0c945ed1abb6ece5b150afed420dd7f2b98e9dbe
cty7574 <vicpool@naver.com>
2026-01-02 17:59:09 +0900

    프로젝트 생성: 15.Animated Splash Screen

---
+++ 15.AnimatedSplashScreen/AnimatedSplashScreen/AnimatedSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSplashScreenApp.swift
+//  AnimatedSplashScreen
+//
+//  Created by 멘태 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 15.AnimatedSplashScreen/AnimatedSplashScreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimatedSplashScreen
+//
+//  Created by 멘태 on 1/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
