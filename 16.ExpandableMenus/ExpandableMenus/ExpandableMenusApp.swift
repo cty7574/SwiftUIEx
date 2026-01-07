b9929071e0bc67b466a9bac7bf9b436312b89d87
cty7574 <vicpool@naver.com>
2026-01-07 16:48:19 +0900

    프로젝트 생성: 16.Expandable Menus

---
+++ 16.ExpandableMenus/ExpandableMenus/ExpandableMenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableMenusApp.swift
+//  ExpandableMenus
+//
+//  Created by 멘태 on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableMenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
