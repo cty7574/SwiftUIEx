0380ac9f47ca4c03027d26863009cf7718762283
cty7574 <vicpool@naver.com>
2026-01-10 21:36:40 +0900

    프로젝트 생성: 17.Complex UI Animations

---
+++ 17.ComplexUIAnimation/ComplexUIAnimation/ComplexUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexUIAnimationApp.swift
+//  ComplexUIAnimation
+//
+//  Created by 멘태 on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ComplexUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
