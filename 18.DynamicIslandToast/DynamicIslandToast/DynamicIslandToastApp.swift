2b90c4029da498bdc98dccd21685246c93a9cac5
cty7574 <vicpool@naver.com>
2026-01-17 21:55:03 +0900

    프로젝트 생성: 18.Dynamic Island Toast

---
+++ 18.DynamicIslandToast/DynamicIslandToast/DynamicIslandToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandToastApp.swift
+//  DynamicIslandToast
+//
+//  Created by 멘태 on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
