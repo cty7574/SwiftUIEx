ea4dd5d339f1b965b815599b2e092771f959a5e0
cty7574 <vicpool@naver.com>
2026-01-19 23:11:26 +0900

    Dynamic Island Toast View 기본 구현(윈도우와 뷰컨트롤러 내부에 위치)

--- 18.DynamicIslandToast/DynamicIslandToast/Modifiers/DynamicIslandToastViewModifier.swift
+++ 18.DynamicIslandToast/DynamicIslandToast/Modifiers/DynamicIslandToastViewModifier.swift
@@ -9,9 +9,39 @@ import SwiftUI
 
 struct DynamicIslandToastViewModifier: ViewModifier {
     @Binding var isPresented: Bool
+    @State private var overlayWindow: PassThroughWindow?
+    @State private var overlayController: CustomHostingView?
+    
     var value: Toast
     
     func body(content: Content) -> some View {
-        content 
+        content
+            .background {
+                WindowExtractor { mainWindow in
+                    createOverlayWindow(mainWindow)
+                }
+            }
     }
+    
+    private func createOverlayWindow(_ mainWindow: UIWindow) {
+        guard let windowScene = mainWindow.windowScene else { return }
+        let overlayWindow = PassThroughWindow(windowScene: windowScene)
+        
+        overlayWindow.backgroundColor = .clear
+        overlayWindow.isHidden = false
+        overlayWindow.isUserInteractionEnabled = true
+        createRootController(overlayWindow)
+        self.overlayWindow = overlayWindow
+    }
+    
+    private func createRootController(_ window: PassThroughWindow) {
+        let hostingController = CustomHostingView(rootView: ToastView())
+        hostingController.view.backgroundColor = .clear
+        window.rootViewController = hostingController
+        self.overlayController = hostingController
+    }
+}
+
+#Preview {
+    ContentView()
 }
